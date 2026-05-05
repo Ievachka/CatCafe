15d829f5ec2b04bad44a59a2a1a8fe123fb84a08
Ievachka <ieva.vinkeviciute@gmail.com>
2026-05-05 20:50:07 +0000

    INTRO CatCafe

---
+++ CatCafe.java
@@ -0,0 +1,7 @@
+public class CatCafe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cat Cafe!");
+        System.out.println("Our cafe is a cozy place where you can enjoy coffee, tea, and snacks. >.<");
+        System.out.println("Come visit us for a warm drink and some purr-fect moments! :3 ");
+    }
+}
