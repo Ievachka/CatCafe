648a9a830fc71c2cd36866197446c007aa27e9b9
Auguste <165314556+Afguste@users.noreply.github.com>
2026-05-18 15:44:29 +0000

    fixed dialog

--- Boss.java
+++ Boss.java
@@ -10,7 +10,7 @@ public class Boss extends Character {
         addDialog(new Dialog("If you complete the tutorial, you will be able to start the next days, upgrade the cafe, and unlock new drinks and snacks!", 4000));
         addDialog(new Dialog("But if you do not complete the tutorial, you will need to start over... or I will just have to fire you.", 4000));
         addDialog(new Dialog("Are you ready?", 1000, Arrays.asList("Yes, let's go!", "No, sorry, I changed my mind.")));
-        addDialog(new Dialog("So, your first task is to serve the customer.", 2000));
+        addDialog(new Dialog("So, your main task is to serve the customer.", 2000));
         addDialog(new Dialog("Read what they want, prepare the item, and give it to them.", 3000));
         addDialog(new Dialog("Be careful. At first, it might be easy, but later customers will order more drinks and snacks.", 4000));
         addDialog(new Dialog("If your order is purrfect - 100% accurate - customers will give you Kitty Tips.", 3000));
